b24ba0765e14ccc417b66697a0ddd13f48d5e8df
Joel Barton <joelbarton406@gmail.com>
2023-11-12 13:44:39 -0700

    utilities_update

--- scratch.js
+++ scratch.js
@@ -1,5 +1,32 @@
-const _ = require('underscore');
-const arr = [1, [2, [3]], 4, 5];
+// const { random } = require("underscore");
 
+const element = ['a', 'b', 'c', 'd', 'e'];
 
-_().without()
+function sample(total = 1) {
+  /*
+  - make copy of element
+  - if copy length <= total, return element
+  - otherwise, do sampling using dynamic copy length
+  */
+  if (!element.length || total < 1) throw new Error('element length or total was 0!');
+  if (element.length <= total) return element;
+
+  const random = (items) => (Math.random() * (items.length - 1)).toFixed();
+  const copy = [...element];
+
+  const samples = [];
+  for (let n = 0; n < total; n += 1) {
+    const idx = random(copy); // get index
+    const item = copy.splice(idx, 1).at(0); // mutate copy
+    console.log(item);
+    samples.push(item); // push item into samples
+  }
+
+  return total === 1 ? samples[0] : samples;
+}
+console.log(sample());
+/*
+- get length of array
+- based on this length, need to select a (random) number between 0 and the length - 1
+- get random is th
+*/
--- utilities_v2.js
+++ utilities_v2.js
@@ -1,16 +1,20 @@
-const _ = function (element) {
+const _ = function lodash(source) {
   const p = console.log;
+
+  const allPropsMatch = function allPropsMatch(target, targetKeys, obj) {
+    return targetKeys.every((key) => (key in obj) && target[key] === obj[key]);
+  };
 
   return {
     first() {
-      return element[0];
+      return source[0];
     },
     last() {
-      return element[element.length - 1];
+      return source[source.length - 1];
     },
     without(...args) {
       const noNoValues = new Set(args);
-      return element.filter((el) => !noNoValues.has(el));
+      return source.filter((el) => !noNoValues.has(el));
     },
     range(...args) {
       // why can't we use `this` here? ->   _().method()   vs   this().method()
@@ -29,25 +33,113 @@ const _ = function (element) {
       return addNumberToArr([0], _(args).first());
     },
     lastIndexOf(searchItem) {
-      for (let index = element.length - 1; index >= 0; index -= 1) {
-        if (element[index] === searchItem) return index;
+      for (let index = source.length - 1; index >= 0; index -= 1) {
+        if (source[index] === searchItem) return index;
       }
       return -1;
     },
-    sample(count) {
-      const random = function random(items) {
+    sample(total = 1) {
+      if (!source.length || total < 1) throw new Error('source length or total was 0!');
+      if (total !== 1 && source.length <= total) return source;
 
-      };
+      const randomIndex = (items) => (Math.random() * (items.length - 1)).toFixed();
+      const copy = [...source];
+      const samples = [];
 
-      if (!arguments.length) {
-        // get random
-      } else {
-        const samples = [];
-        // get random `count` times from element
+      for (let n = 0; n < total; n += 1) {
+        const idx = randomIndex(copy);
+        const item = copy.splice(idx, 1).at(0);
+        samples.push(item);
       }
+
+      return total === 1 ? samples[0] : samples;
+    },
+    findWhere(target) {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const sourceObj of source) {
+        if (allPropsMatch(target, Object.keys(target), sourceObj)) return sourceObj;
+      }
+      return undefined;
+    },
+    where(target) {
+      return source.filter((sourceObj) => allPropsMatch(target, Object.keys(target), sourceObj));
+    },
+    pluck(targetKey) {
+      return source.reduce((result, item) => {
+        if (Object.prototype.hasOwnProperty.call(item, targetKey)) {
+          const value = item[targetKey];
+          result.push(value);
+        }
+        return result;
+      }, []);
+    },
+    keys() {
+      return Object.getOwnPropertyNames(source);
+    },
+    values() {
+      const keys = _(source).keys(); // reuse keys
+      const { length } = keys;
+      const values = Array(length);
+      for (let i = 0; i < length; i += 1) {
+        values[i] = source[keys[i]];
+      }
+      return values;
+    },
+    extend(...objects) {
+      const result = objects.shift();
+      while (objects.length) {
+        const curr = objects.pop();
+        const currKeys = _(curr).keys();
+        currKeys.forEach((key) => {
+          result[key] = curr[key];
+        });
+      }
+
+      return result;
+    },
+    pick(...selectedKeys) {
+      return selectedKeys.reduce((result, key) => {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          // strange that eslint doesn't recognize this creates a property on an object...
+          // eslint-disable-next-line no-param-reassign
+          result[key] = source[key];
+        }
+        return result;
+      }, {});
+    },
+    omit(...excludedKeys) {
+      return excludedKeys.reduce((result, key) => {
+        if (Object.prototype.hasOwnProperty.call(result, key)) {
+          // eslint-disable-next-line no-param-reassign
+          delete result[key];
+        }
+        return result;
+      }, source);
+    },
+    has(someKey) {
+      return Object.prototype.hasOwnProperty.call(source, someKey);
+    },
+    isElement(arg) {
+
+    },
+    isArray(arg) {
+
+    },
+    isObject(arg) {
+
+    },
+    isFunction(arg) {
+
+    },
+    isBoolean(arg) {
+
+    },
+    isString(arg) {
+
+    },
+    isNumber(arg) {
+
     },
   };
 };
-/*
 
-*/
